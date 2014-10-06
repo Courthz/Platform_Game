174aadccb5d062e6d5885413e92732ca53bfcae5
Courthz <danielcasajustvjb32013@cev.com>
2014-10-06 23:06:25 +0200

    Platform_Unity
    Platform game in Unity3D based on C# coding.

---
+++ Assets/Scripts/Player/ColisionPJ.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColisionPJ : MonoBehaviour {
+	
+
+	public GameObject blood;
+	//public GameObject coin;
+	//public TextMesh counterCoins;
+	public DetectDeadZone dead;
+	//int counterCoinss = 0;
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Enemy"){
+			Debug.Log ("112");
+			//Instanciar la sangre
+			GameObject bl = (GameObject) Instantiate(blood, transform.position, Quaternion.identity); //crea un gameobject en escena
+			
+			//Restar vida
+			//textLife.text = (int.Parse(textLife.text) - 25).ToString();
+			//Destruir la sangre pasado un tiempo
+			Destroy (bl, 1);
+			
+			//Destruir la piedra
+			Destroy (other.gameObject);
+
+			dead.GameOver ();
+			
+			
+		}
+		
+		/*if (other.tag == "Coin") {
+			GameObject cn = (GameObject) Instantiate (coin, transform.position, Quaternion.identity);
+			
+			Destroy (cn, 1);
+			
+			Destroy (other.gameObject);
+
+			counterCoins.text = (counterCoinss += 1).ToString();
+		}*/	
+
+		if ((other.tag == "Boss") || (other.tag == "Enemy")) {
+
+			dead.GameOver ();
+
+		}
+	}
+}
---
+++ Assets/Scripts/Player/MoveConstructor.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveConstructor : MonoBehaviour {
+	
+	public float speed = 6.0F;
+	public float jumpSpeed = 8.0F;
+	public float gravity = 20.0F;
+	private Vector3 moveDirection = Vector3.zero;
+	private AnimationCharacter animation;
+	private BoxCollider colision;
+	private bool godMode;
+
+	private DataLogic dataLogic;
+
+	void Start() {
+
+		godMode = false;
+		dataLogic = GameObject.FindGameObjectWithTag("DataLogic").GetComponent<DataLogic>();
+		animation = transform.FindChild("Constructor").GetComponent<AnimationCharacter>();
+		colision = transform.FindChild("Collider").GetComponent<BoxCollider>();
+	}
+
+	void Update() { //Actualiza los frames
+
+		Camera.main.transform.position = new Vector3 (transform.position.x, transform.position.y + 1.5f, -6);
+		
+		CharacterController controller = GetComponent<CharacterController>();
+		if (godMode == false) {
+			gravity = 20.0F;
+			if (Input.GetKey ("o")) godMode = true;
+			if (controller.isGrounded) { //controla si esta pisando el suelo
+				//Debug.Log ("PISANDO");
+				
+				moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+				moveDirection = transform.TransformDirection(moveDirection);
+				moveDirection *= speed;
+
+				if (moveDirection.x>0) animation.SetRunRight();
+				else if (moveDirection.x<0) animation.SetRunLeft();
+				else animation.SetIdle();
+				
+				if (Input.GetButton("Jump")) {
+					dataLogic.Play (dataLogic.jump);
+					moveDirection.y = jumpSpeed;
+					animation.SetJump();
+				}
+			}
+		} 
+		if (godMode == true) {
+			gravity = 0F;
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis ("Vertical"), 0);
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			
+			if (moveDirection.x>0) animation.SetRunRight();
+			else if (moveDirection.x<0) animation.SetRunLeft();
+			else animation.SetIdle();
+
+			if (Input.GetKey ("o")) godMode = false;
+			
+			/*if (Input.GetButton("Jump")) {
+				moveDirection.y *= speed;
+			}*/
+		
+		}
+		if (Input.GetKey ("p")) colision.enabled = !colision.enabled;
+		//speed += 0.01f;
+		moveDirection.y -= gravity * Time.deltaTime; //El delta time controla los frames por segundo
+		moveDirection.z = 0;
+		//moveDirection.x = 100 * Time.deltaTime*speed;
+		controller.Move(new Vector3(moveDirection.x * Time.deltaTime,moveDirection.y * Time.deltaTime,0)); //se mueve el personaje
+		
+	}
+}
